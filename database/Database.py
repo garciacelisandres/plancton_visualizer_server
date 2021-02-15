2e91ea37b8c00fb59c969889884f2f1c7a031dc6
garciacelisandres <garciacelisandres@gmail.com>
2021-02-15 18:14:35 +0100

    Changed methods in file to properly handle cursors.
    - Also added code to use correct operators to find the searched for contents.

--- database/Database.py
+++ database/Database.py
@@ -3,27 +3,30 @@ def get_samples(mongo, sample_classes, start_time, end_time):
         raise ValueError
     try:
         find_dict = {
-            "start_time": {"$ge": start_time},
-            "end_time": {"$le": end_time}
+            "$and": [
+                {"date_retrieved": {"$gte": start_time}},
+                {"date_retrieved": {"$lte": end_time}}
+            ]
         }
-        if not(sample_classes is None or len(sample_classes) == 0):
+        if not (sample_classes is None or len(sample_classes) == 0):
             find_dict["sample_classes"] = {"$in": sample_classes}
-        mongo.samples.find(find_dict)
-    except:
+        samples_list = [sample for sample in mongo.samples.find(find_dict)]
+        return samples_list
+    except Exception as e:
         raise InterruptedError
 
 
 def get_classes(mongo):
     try:
-        class_list = mongo.classes.find({})
+        class_list = [class_obj for class_obj in mongo.classes.find({})]
         return class_list
-    except:
+    except Exception:
         raise InterruptedError
 
 
 def get_class(mongo, class_id):
     try:
-        class_obj = mongo.classes.find({"_id": class_id})
+        class_obj = mongo.classes.find_one({"_id": class_id})
         return class_obj
-    except:
+    except Exception:
         raise InterruptedError
